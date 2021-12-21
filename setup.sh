a766e87023c7d702bc769077c7ca5f26ac3a0e6f
Jeroen Venema <jeroen.venema@gmail.com>
2021-12-21 15:58:49 +0100

    Plug installed

--- setup.sh
+++ setup.sh
@@ -14,18 +14,9 @@ fi
 ln -s ~/home_dotfiles/.vimrc ~/.vimrc
 ln -s ~/home_dotfiles/.bashrc ~/.bashrc
 
-# install VIM plugins
-echo Installing VIM plugins..
-cd 
-if [ -d ".vim/pack/vendor/start" ]; then
-    echo Removing old plugins..
-    rm -rf .vim/pack/vendor/start
-fi
-mkdir -p ~/.vim/pack/vendor/start 
-echo Installing new plugins from github..
-git clone --quiet --depth 1 https://github.com/spf13/vim-autoclose.git ~/.vim/pack/vendor/start/autoclose 
-git clone --quiet --depth 1 https://github.com/itchyny/lightline.vim.git ~/.vim/pack/vendor/start/lightline 
-git clone --quiet --depth 1 https://github.com/tpope/vim-surround.git ~/.vim/pack/vendor/start/vim-surround 
-git clone --quiet --depth 1 https://github.com/maxbane/vim-asm_ca65.git ~/.vim/pack/vendor/start/vim-asm_ca65
+# install plugin manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo Setup complete
+echo Run :PlugInstall in vim now
